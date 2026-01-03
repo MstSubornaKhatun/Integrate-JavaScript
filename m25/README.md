36a6f14d5260af463f19b6cc695e4cf81e90efc2
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-03 11:54:39 +0600

    first commit

---
+++ m25/README.md
@@ -0,0 +1,1 @@
+## forget and move on!
---
+++ m25/js/dom.js
@@ -0,0 +1,25 @@
+console.log('hello form js');
+console.log(document); // html er jinish gulo avabe javascript ar modhe paoya jai.
+console.log(document.body);
+
+
+/*
+live server theke inspect kore 
+document --> likhe enter koro
+document.body --> likhe enter koro
+
+document likhe dot dile soto soto property dekha jabe.
+(document.)
+
+document.getElementByTagName -->
+document.getElementsByTagName() DOM 
+থেকে নির্দিষ্ট tag name অনুযায়ী সব 
+elements খুঁজে বের করে এবং 
+HTMLCollection আকারে রিটার্ন করে। 
+JavaScript দিয়ে index ব্যবহার করে 
+এগুলো manipulate করা যায়।
+
+
+
+
+*/ 
